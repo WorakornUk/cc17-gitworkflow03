bdd1b62542540d254cf09c9b32ebf68834da22cd
WorakornUk <worakorn.uk@gmail.com>
2024-06-19 14:58:10 +0700

    create Register

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
-
 const app = express()
+
+app.use('/register', (req, res) => {
+  res.json({ msg: 'Register...' })
+})
+
 
 app.listen(8000, ()=>{ console.log('run server')})
