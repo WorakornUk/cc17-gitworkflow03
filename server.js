94389984c3971311caee94ece17b6c715c5e273a
drivezz22 <puwanut.pansailom@gmail.com>
2024-06-19 15:02:52 +0700

    feat : add login feature

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use('/register', (req, res) => {
-  res.json({ msg: 'Register...' })
-})
+app.use("/register", (req, res) => {
+  res.json({ msg: "Register..." });
+});
 
+app.use("/login", (req, res) => {
+  res.json({ msg: "Login......" });
+});
 
-app.listen(8000, ()=>{ console.log('run server')})
+app.listen(8000, () => {
+  console.log("run server");
+});
